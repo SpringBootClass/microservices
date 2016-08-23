85bc9732faac22c5bfbde29a4e4ad105f5428bc2
Siva Prasad Bhushetty <sivaprasad1110@gmail.com>
2016-08-23 10:41:37 -0400

    deleted irrelevant files

--- OnetoManyHibernate/bin/data/mysql/data.sql
+++
@@ -1,4 +0,0 @@
---insert into carts(total,name) values(1000,'mycart');
---insert into items(item_id,item_total,quantity) values('tv_001',80,1);
---insert into items(item_id,item_total,quantity) values('fridge_001',400,2);
---insert into items(item_id,item_total,quantity) values('homestudio_001',200,1);
